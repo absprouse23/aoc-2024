2dc680d8bf21b4c83f82769f3fc8afad137de791
absprouse23 <trainguy2004@gmail.com>
2024-12-05 15:01:15 -0500

    finish part 2 day 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -4,61 +4,98 @@ use std::io;
 use std::io::BufRead;
 use std::path::Path;
 
-fn main() {
+fn main() -> io::Result<()> {
     let input_path = Path::new("input.txt");
 
-    match part_one(input_path) {
+    let lines: Vec<String> = io::BufReader::new(File::open(input_path)?)
+        .lines()
+        .map(|l| l.unwrap())
+        .collect();
+
+    match part_one(&lines) {
         Ok(v) => println!("Part 1: {}", v),
         Err(e) => println!("{}", e),
     }
-}
-fn part_one(input_path: &Path) -> Result<i32, Box<dyn Error>> {
-    let mut safe_reports = 0;
-    let lines = io::BufReader::new(File::open(input_path)?)
-        .lines()
-        .map(|l| l.unwrap());
 
-    for line in lines {
+    match part_two(&lines) {
+        Ok(v) => println!("Part 2: {}", v),
+        Err(e) => println!("{}", e),
+    }
+    Ok(())
+}
+fn part_one(input: &[String]) -> Result<i32, Box<dyn Error>> {
+    let mut safe_reports = 0;
+
+    for line in input {
         let curr: Vec<i32> = line
             .split_whitespace()
-            .map(|s| s.parse::<i32>())
-            .collect::<Result<Vec<i32>, _>>()?;
+            .map(|s| s.parse().expect("Not a number"))
+            .collect();
 
-        let mut safe = true;
-        let mut increasing = false;
-        let mut decreasing = false;
-
-        for i in 1..curr.len() {
-            let diff = curr[i] - curr[i - 1];
-
-            if diff == 0 {
-                safe = false;
-                break;
-            }
-            
-            if diff > 0 {
-                increasing = true;
-            } else {
-                decreasing = true;
-            }
-
-            if !(1..=3).contains(&diff.abs()) {
-                safe = false;
-                break;
-            }
-        }
-        
-        if decreasing == increasing {
-            safe = false;
-        }
-
-        if safe {
-            println!("{} Safe", line);
+        if test_line(&curr) {
             safe_reports += 1;
-        } else {
-            println!("{} Unsafe", line);
         }
     }
 
     Ok(safe_reports)
+}
+
+fn part_two(input: &[String]) -> Result<i32, Box<dyn Error>> {
+    let mut safe_reports = 0;
+
+    for line in input {
+        let curr: Vec<i32> = line
+            .split_whitespace()
+            .map(|s| s.parse().expect("Not a number"))
+            .collect();
+
+        if test_line(&curr) {
+            safe_reports += 1;
+        } else {
+            let mut test_vecs: Vec<Vec<i32>> = vec![];
+            
+            for i in 0..curr.len() {
+                let mut test_vec: Vec<i32> = curr.clone();
+                test_vec.remove(i);
+                test_vecs.push(test_vec);
+            }
+            
+            for vec in &test_vecs {
+                if test_line(vec) {
+                    safe_reports += 1;
+                    break;
+                }
+            }
+        }
+    }
+
+    Ok(safe_reports)
+}
+
+fn test_line(curr: &[i32]) -> bool {
+    let mut increasing = false;
+    let mut decreasing = false;
+
+    for i in 1..curr.len() {
+        let diff = curr[i] - curr[i - 1];
+
+        if diff == 0 {
+            return false;
+        }
+
+        if diff > 0 {
+            increasing = true;
+        } else {
+            decreasing = true;
+        }
+
+        if !(1..=3).contains(&diff.abs()) {
+            return false;
+        }
+    }
+
+    if decreasing == increasing {
+        return false;
+    }
+    true
 }
