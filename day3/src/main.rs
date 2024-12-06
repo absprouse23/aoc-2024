726542a6dabe6bdcb36f549565ab5741982ef7d4
absprouse23 <trainguy2004@gmail.com>
2024-12-06 10:41:16 -0500

    finish part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -2,10 +2,11 @@ use regex::Regex;
 
 fn main() {
     let data = std::fs::read_to_string("input.txt").expect("failed to read file");
-    println!("{}", part_one(data));
+    println!("Part 1: {}", part_one(&data));
+    println!("Part 2: {}", part_two(&data));
 }
 
-fn part_one(data: String) -> i32 {
+fn part_one(data: &String) -> i32 {
     let re = Regex::new(r"mul\([0-9]+,[0-9]+\)").unwrap();
     let valid_muls: Vec<&str> = re
         .find_iter(&data)
@@ -16,6 +17,30 @@ fn part_one(data: String) -> i32 {
     for instr in valid_muls {
         result += parse_mul(instr.parse().unwrap());
     }
+    result
+}
+
+fn part_two(data: &String) -> i32 {
+    let mut result = 0;
+    let re = Regex::new(r"(don't\(\))|(do\(\))|(mul\([0-9]+,[0-9]+\))").unwrap();
+    let instructions: Vec<&str> = re
+        .find_iter(&data)
+        .map(|cap| cap.as_str())
+        .collect();
+
+    let mut ignore_mul = false;
+
+    for instr in instructions {
+        match instr {
+            "do()" => ignore_mul = false,
+            "don't()" => ignore_mul = true,
+            _ => match ignore_mul{
+                true => {},
+                false => {result += parse_mul(instr.parse().unwrap())}
+            }
+        }
+    }
+
     result
 }
 
